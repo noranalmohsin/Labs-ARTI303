5c79a681e3f1c4b9d2790a87d179b9e5defb68c5
Noran Al-Mohsin <noranalmohsin@gmail.com>
2026-09-09 15:22:15 +0300

    Add Lab 2

---
+++ notebooks/grade_utils.py
@@ -0,0 +1,15 @@
+"""TODO: describe what this module is for."""
+
+def letter_grade(gpa):
+    """TODO: describe what this function does."""
+    # TODO: your if/elif chain here
+    if gpa >= 4.50:
+        return "A"
+    elif gpa >= 3.50:
+        return "B"
+    elif gpa >= 2.50:
+        return "C"
+    elif gpa >= 1.50:
+        return "D"
+    else:
+        return "F"
